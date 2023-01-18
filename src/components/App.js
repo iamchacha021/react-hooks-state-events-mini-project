c5f03473247bfa8d7d97fc3f33ad2f4465df50b8
iamchacha021 <iamchacha021@gmail.com>
2023-01-18 23:53:39 +0300

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -4,16 +4,32 @@ import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
 
 import { CATEGORIES, TASKS } from "../data";
+import { useState } from "react";
 console.log("Here's the data you're working with");
 console.log({ CATEGORIES, TASKS });
 
+
 function App() {
+  const [tasks, setTasks] = useState(TASKS)
+  const [categories, setCategories]= useState(CATEGORIES)
+
+  const handleDelete = (clickedTask)=>{
+    const filteredTask = tasks.filter((task)=>{
+      return clickedTask.text !== task.text
+    })
+    setTasks(filteredTask)
+  }
+
+  const onTaskFormSubmit = (task)=>{
+    setTasks([...tasks, task])
+  }
+
   return (
     <div className="App">
       <h2>My tasks</h2>
       <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <NewTaskForm categories={categories} onTaskFormSubmit={onTaskFormSubmit}/>
+      <TaskList tasks={tasks}  handleDelete={handleDelete} />
     </div>
   );
 }
--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,16 +1,34 @@
 import React from "react";
+import { useState } from "react";
 
-function NewTaskForm() {
+function NewTaskForm({categories, onTaskFormSubmit}) {
+  const [details, setDetails] = useState("")
+  const [category, setCategory] = useState("All")
+
+  const handleSubmit = (ev)=>{
+    ev.preventDefault()
+    onTaskFormSubmit({text:details, category:category})
+  }
+
   return (
-    <form className="new-task-form">
+    <form className="new-task-form" onSubmit={handleSubmit}>
       <label>
         Details
-        <input type="text" name="text" />
+        <input 
+          type="text" 
+          name="text" 
+          value={details}
+          onChange={(ev)=> setDetails(ev.target.value)}
+        />
       </label>
       <label>
         Category
-        <select name="category">
-          {/* render <option> elements for each category here */}
+        <select name="category" value={category} onChange={(ev)=> setCategory(ev.target.value)}>
+          {categories.map((category)=>{
+            return(
+              <option key={category} value={category}>{category}</option>
+            )
+          })}
         </select>
       </label>
       <input type="submit" value="Add task" />
